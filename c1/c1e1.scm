d505c451ef506650a3ce99cdf0e78850d7e70c8c
xiaoyugitcode <xiaoyulink@gmail.com>
2013-08-20 21:57:19 +0800

    注释应该使用分号，之前使用的是冒号

--- c1/c1e1.scm
+++ c1/c1e1.scm
@@ -1,4 +1,4 @@
-: This is a Scheme comment
-: MIT Scheme 的基本使用 http://www.math.pku.edu.cn/teachers/qiuzy/progtech/scheme/mit_scheme.htm
-：基本的使用概括起来说就是：编辑器编辑代码；在MIT Scheme解释器中使用(load "file")的方式载入代码并运行。
-：c1e1照书上敲一遍代码就可以了，这里就不提了。
+; In a Scheme program, any line that begins with one or more semicolons is a comment
+; MIT Scheme 的基本使用见 http://www.math.pku.edu.cn/teachers/qiuzy/progtech/scheme/mit_scheme.htm
+; 基本的使用概括起来说就是：编辑器编辑代码；在MIT Scheme解释器中使用(load "file")的方式载入代码并运行。
+; c1e1照书上敲一遍代码就可以了。
