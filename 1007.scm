452c2891dd569d6bf9768ce4204eeb8eefbf8965
xiaoyuwk <xiaoyulink@gmail.com>
2014-05-21 22:57:14 +0800

    add 1007.md

--- 1007.scm
+++ 1007.scm
@@ -19,6 +19,8 @@
 (define (square x)
   (* x x))
 
+;num 表示初始值，test-times表示进行test-times次测试,每次测试将num平方作为新的num
+;每进行一次测试，先显示num的平方，然后显示num最后显示xysqrt对num的平方进行的开方操作
 (define (sqrt-test num test-times)
   (begin (display (* num num)) (newline) (display num) (newline) (display (xysqrt (* num num))) (newline) (newline)
          (if (> test-times 0)
