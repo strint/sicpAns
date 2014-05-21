20f3feb205b0f1a7e3dfe49493e7a8e4a87b55bc
xiaoyuxu <xiaoyulink@gmail.com>
2014-05-21 21:17:23 +0800

    add 1007_b

--- 1007_b.scm
+++ 1007_b.scm
@@ -1,27 +1,23 @@
 (define (xysqrt in)
-  (sqrt-iter 1.0 in))
+  (sqrt-iter 1.0 in in))
 
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+(define (sqrt-iter guess pre-guess x)
+  (if (good-enough? guess pre-guess)
       guess
-      (sqrt-iter (improve guess x) x)))
+      (sqrt-iter (improve guess x) guess x)))
 
 (define (improve g b)
   (/ (+ g
         (/ b g))
      2.0))
 
-(define (good-enough? c d)
-  (< (abs (- (square c)
-             d))
-     0.001))
-
-(define (square x)
-  (* x x))
+(define (good-enough? guess pre-guess)
+  (< (/ (abs (- guess pre-guess))
+        pre-guess)
+     0.0001))
 
 (define (sqrt-test num test-times)
   (begin (display (* num num)) (newline) (display num) (newline) (display (xysqrt (* num num))) (newline) (newline)
          (if (> test-times 0)
            (sqrt-test (* num num) (- test-times 1))
-           num)))
-
+           (display test-times))))
