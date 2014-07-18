3abd671574b89fa27ee3bfa97e3349fd5b58ff05
xiaoyugitcode <xiaoyulink@gmail.com>
2014-07-19 01:51:23 +0800

    add explaination for 1045

--- 1045_nth_root.scm
+++ 1045_nth_root.scm
@@ -1,3 +1,12 @@
+(define (i-expt b n)
+  (define (iter-expt b n a k c)
+    (if (= n 0)
+        c
+        (cond ((< (- n (* 2 k)) 0) (iter-expt b (- n k) b 1 (* a c))) ;n=[may have some 0,may have some 1]1 or [must have some 1, may have some 0]0 (binary)
+              ((= (- n (* 2 k)) 0) (iter-expt b 0 b 0 (* a a c))) ;n=1[all 0] (binary)
+              (else (iter-expt b n (* a a) (* 2 k) c)))))
+  (iter-expt b n b  1 1))
+
 (define (repeated f n)
   (define (repe-iter g i)
     (if (> i 1)
@@ -20,14 +29,6 @@
         (try next))))
   (try first-guess))
 
-(define (i-expt b n)
-  (define (iter-expt b n a k c)
-    (if (= n 0)
-        c
-        (cond ((< (- n (* 2 k)) 0) (iter-expt b (- n k) b 1 (* a c))) ;n=[may have some 0,may have some 1]1 or [must have some 1, may have some 0]0 (binary)
-              ((= (- n (* 2 k)) 0) (iter-expt b 0 b 0 (* a a c))) ;n=1[all 0] (binary)
-              (else (iter-expt b n (* a a) (* 2 k) c)))))
-  (iter-expt b n b  1 1))
 
 (define (nth-root x nth k)
   (fixed-point (repeated (average-damp (lambda (y) (/ x (i-expt y (- nth 1))))) k) 1))
