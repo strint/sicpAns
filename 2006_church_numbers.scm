439f108f38041911d64306c94afab0f34a16417e
xiaoyugitcode <xiaoyulink@gmail.com>
2014-08-01 00:12:27 +0800

    add more to 2006

--- 2006_church_numbers.scm
+++ 2006_church_numbers.scm
@@ -1,3 +1,15 @@
-(define zero (lambda (f) (lambda (x) x)))
+(define zero (lambda (f) (lambda (x) x))) ;turn f into procedure (lambda (x) x)
 (define (add-1 n)
-  (lambda (f) (lambda (x) (f ((n f) x)))))
+  (lambda (f) (lambda (x) (f ((n f) x))))) ;
+
+; (add-1 zero)
+;(define one (add-1 zero))
+;(define one (lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x)))))
+;(define one (lambda (f) (lambda (x) (f ((lambda (x) x) x)))))
+(define one (lambda (f) (lambda (x) (f x))))
+
+;(add-1 one)
+;(define two (add-1 one))
+;(define two (lambda (f) (lambda (x) (f ((one f) x)))))
+;(define two (lambda (f) (lambda (x) (f ((lambda (x) (f x)) x)))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
