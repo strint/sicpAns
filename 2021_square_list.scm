728a1aff02efadca4d55e49383f6ad3b07edfa75
xiaoyugitcode <xiaoyulink@gmail.com>
2014-08-08 20:59:48 +0800

    add 2021

---
+++ 2021_square_list.scm
@@ -0,0 +1,7 @@
+(define (square-list1 items)
+  (if (null? items)
+    '()
+    (cons (square (car items))
+          (square-list1 (cdr items)))))
+(define (square-list2 items)
+  (map square items))
