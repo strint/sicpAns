2283f717447fe7cb9110274500ea7a1407eac3dd
xiaoyugitcode <xiaoyulink@gmail.com>
2014-11-15 01:52:24 +0800

    add 2047

---
+++ 2/2047_frame_selectors.scm
@@ -0,0 +1,18 @@
+; origin-frame, edge1-frame, and edge2-frame
+(define (make-frame1 origin edge1 edge2)
+  (list origin edge1 edge2))
+(define (origin-frame1 f)
+  (car f))
+(define (edge1-frame1 f)
+  (car (cdr f)))
+(define (edge2-frame1 f)
+  (car (cdr (cdr f))))
+
+(define (make-frame2 origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+(define (origin-frame2 f)
+  (car f))
+(define (edge1-frame2 f)
+  (car (cdr f)))
+(define (edge2-frame2 f)
+  (cdr (cdr f)))
