fd77e879fce1fbea1cd946e3f696fc8dc2ed6f71
xiaoyugitcode <xiaoyulink@gmail.com>
2014-11-15 00:35:21 +0800

    add 2045

---
+++ 2/2045_split.scm
@@ -0,0 +1,6 @@
+(define (split outer-op inner-op)
+  (lambda (painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller ((split out-op inner-op) painter (- n 1)))) ; 有一点疑惑：这样的递归operation，如此定义是否正确
+          (outer-op painter (inner-op smaller smaller))))))
