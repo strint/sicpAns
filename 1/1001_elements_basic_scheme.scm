2278aff1447c24cc6264d1bd9348581186933c33
strint <xiaoyulink@gmail.com>
2015-07-08 08:51:11 +0800

    Update and rename 1001-basic-scheme.scm to 1001_elements_basic_scheme.scm

--- 1/1001_elements_basic_scheme.scm
+++ 1/1001_elements_basic_scheme.scm
@@ -2,3 +2,35 @@
 ; MIT Scheme 的基本使用见 http://www.math.pku.edu.cn/teachers/qiuzy/progtech/scheme/mit_scheme.htm
 ; 基本的使用概括起来说就是：编辑器编辑代码；在MIT Scheme解释器中使用(load "file")的方式载入代码并运行。
 ; c01e01照书上敲一遍代码就可以了。
+10
+
+(+ 5 3 4)
+
+(- 9 1)
+
+(/ 6 2)
+
+(+ (* 2 4) (- 4 6))
+
+(define a 3)
+
+(define b (+ a 1))
+
+(+ a b (* a b))
+
+(= a b)
+
+(if (and (> b a) (< b (* a b)))
+    b
+    a)
+
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+
+(+ 2 (if (> b a) b a))
+
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1))
